c68e03f49a6d9d94752f7cca7afd73a42a941ee9
Lozmep <lozano.mauriciop@gmail.com>
2023-07-22 00:29:49 -0500

    add exercise 2 solution

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+const fs = require('node:fs/promises')
+const path = require('node:path')
+
 // Ejercicio 2
 async function writeFile (filePath, data, callback) {
+  const dirName = path.dirname(filePath)
+  let isFolderCreated
 
+  try {
+    await fs.stat(dirName)
+    isFolderCreated = true
+  } catch {
+    isFolderCreated = false
+  }  
+  
+  if (!isFolderCreated) {
+    try {
+      await fs.mkdir(dirName, { recursive: true })
+    } catch(err) {
+      console.log(`Error al crear el directorio: ${err}`)
+      process.exit(1)
+    }
+  }
+    
+  fs.writeFile(filePath, data)
+    .then(() => {
+      callback
+    })
+    .catch(() => {
+      console.log(`Error al crear el archivo: ${err}`)
+      process.exit(1)
+    })
 }
 
 // Ejercicio 3
